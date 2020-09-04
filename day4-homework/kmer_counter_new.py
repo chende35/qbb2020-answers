9fdad02c228f3d1e84488db77a747d2863b0f98d
Catherine Henderson <chende35@jhu.edu>
2020-09-04 10:25:23 -0400

    added comments

--- day4-homework/kmer_counter_new.py
+++ day4-homework/kmer_counter_new.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+# so that the computer knows how to read this
 import sys
+#so that the user can input info from the command line
 from fasta_iterator_class import FASTAReader
+#we reference these functions
+
 def main():
     reader = FASTAReader(open(sys.argv[1]))
+    # so that the target file is the first argument
+    
     kmers = {}
     k = int(sys.argv[3])
+    # so that the kmer size is the third argument
+    
     for seq_id, sequence in reader:
         for i in range(0, len(sequence) - (k + 1)):
             kmer = sequence[i:(i + k)]
@@ -12,27 +20,26 @@ def main():
             if kmers.get(kmer) == None:
                 kmers.setdefault(kmer, 0)
                 kmers[kmer] = (seq_id, i)
+                
             else:
                 kmers[kmer] += (seq_id,i)
-    #for key, value in kmers.items():
-     #   print("{}: {}".format(key, kmers[key]))
         
         
     reader_2 = FASTAReader(open(sys.argv[2]))
+    #so the querry seq is the third argument
+    
     k = int(sys.argv[3])
-   # count = 0
+  
     for seq_id, sequence in reader_2:
+        
         for i in range(0, len(sequence) - k + 1):
             kmer_2 = sequence[i:(i + k)]
+            
             if kmers.get(kmer_2) == None:
-               # count += 1
                 continue
+                
             else:
-                #master_list.append((kmers[kmer_2], kmers, i, kmer_2))
                 print(kmers.get(kmer_2), i, kmer_2)
-                #instead of appending, define vars and print indiv
-                #if count >10000:
-                #    break
 
         
 if __name__ == "__main__":
